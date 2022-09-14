b2b30bd7008b30d24967febea21e204518040612
Robert Shih <robertshih@google.com>
2022-09-14 18:58:23 +0200

    ginkgo: Enable AIDL Clearkey HAL
    Signed-off-by: DarkJoker360 <simoespo159@gmail.com>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- device.mk
+++ device.mk
@@ -155,7 +155,7 @@ TARGET_SCREEN_WIDTH := 1080
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.4-service.clearkey \
+    android.hardware.drm-service.clearkey \
     android.hardware.drm@1.3.vendor
 
 # EPPE
