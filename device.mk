9105582d17bc3918f5c5b93e10e89eb20b9be484
Michael Bestas <mikeioannina@gmail.com>
2024-06-14 00:26:49 +0300

    ginkgo: Build missing libraries for 14 QPR3
    Change-Id: I6a84526ca1e9f4e5140c8068646433cab3fe85b2
    Signed-off-by: tejas101k <tejassingh649@rediffmail.com>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- device.mk
+++ device.mk
@@ -38,6 +38,7 @@ PRODUCT_PACKAGES += \
     audio.usb.default \
     libaudio-resampler \
     libaudioroute \
+    libaudioroute.vendor \
     libtinycompress \
     libvolumelistener
 
@@ -212,8 +213,10 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     libhidltransport.vendor \
+    libhwbinder \
     libhwbinder.vendor \
-    fastcharge.sh
+    libhidlmemory.vendor \
+    fastcharge.sh 
 
 # IMS
 PRODUCT_PACKAGES += \
@@ -241,7 +244,8 @@ PRODUCT_COPY_FILES += \
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0.vendor \
-    android.hardware.keymaster@4.1.vendor
+    android.hardware.keymaster@4.1.vendor \
+    libion.vendor
 
 # Keylayouts
 PRODUCT_COPY_FILES += \
@@ -370,7 +374,8 @@ PRODUCT_PACKAGES += \
     libjson \
     libprotobuf-cpp-full \
     libprotobuf-cpp-full-3.9.1-vendorcompat \
-    librmnetctl
+    librmnetctl \
+    libsqlite.vendor:64
 
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.5.vendor \
@@ -499,3 +504,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.passpoint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.passpoint.xml \
     frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
+
+# WiFi Display
+PRODUCT_PACKAGES += \
+    libpng.vendor \
+    libprocessgroup.vendor
