6cf3d9acd84ff59ae8ae13e6e0c741e0467dbe5f
LuK1337 <priv.luk@gmail.com>
2024-11-06 11:56:59 +0100

    ginkgo: Use common lib_fixups
    Change-Id: I862e547f3375184729eb1a909bb7d6f676d45dfe
    Signed-off-by: Skyblueborb <tomaszborbely0710@gmail.com>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -7,11 +7,8 @@ from extract_utils.fixups_blob import (
 )
 from extract_utils.fixups_lib import (
     lib_fixup_remove,
-    lib_fixup_remove_arch_suffix,
-    lib_fixup_vendorcompat,
+    lib_fixups,
     lib_fixups_user_type,
-    libs_clang_rt_ubsan,
-    libs_proto_3_9_1,
 )
 from extract_utils.main import (
     ExtractUtils,
@@ -30,8 +27,7 @@ namespace_imports = [
 def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
     return f'{lib}_{partition}' if partition == 'vendor' else None
 lib_fixups: lib_fixups_user_type = {
-    libs_clang_rt_ubsan: lib_fixup_remove_arch_suffix,
-    libs_proto_3_9_1: lib_fixup_vendorcompat,
+    **lib_fixups,
     (
         'com.qualcomm.qti.dpm.api@1.0',
         'libmmosal',
