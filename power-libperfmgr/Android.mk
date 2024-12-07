70e66c85e96ec26bfe6c0a54ba3b21fb5bfc8bbe
Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>
2024-12-07 19:09:47 +0800

    Revert "ginkgo: power-libperfmgr: Disable LOCAL_VINTF_FRAGMENTS"
    This reverts commit 6f854dc8739b8a59cb187a059ba105d6936c84ee.
    
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- power-libperfmgr/Android.mk
+++ power-libperfmgr/Android.mk
@@ -47,5 +47,6 @@ LOCAL_MODULE := android.hardware.power-service.xiaomi-libperfmgr
 LOCAL_INIT_RC := android.hardware.power-service.xiaomi-libperfmgr.rc
 LOCAL_MODULE_TAGS := optional
 LOCAL_VENDOR_MODULE := true
+LOCAL_VINTF_FRAGMENTS := android.hardware.power-service.xiaomi.xml
 
 include $(BUILD_EXECUTABLE)
